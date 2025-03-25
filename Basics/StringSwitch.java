31dda33596233f8f6b01eda875f80b9c441610fa
Khushi Malhotra <malhotrakhushi507@gmail.com>
2025-03-26 00:48:52 +0530

    switch cases

--- Basics/StringSwitch.java
+++ Basics/StringSwitch.java
@@ -2,24 +2,15 @@ public class StringSwitch {
     public static void main(String[] args) {
         // Declaring String variable
         String levelString = "Expert";
-        int level = 0;
+        int level;
 
         // Using String in Switch expression
-        switch (levelString) {
-            // Using String literals in Switch case
-            case "Beginner":
-                level = 1;
-                break;
-            case "Intermediate":
-                level = 2;
-                break;
-            case "Expert":
-                level = 3;
-                break;
-            default:
-                level = 0;
-                break;
-        }
+        level = switch (levelString) {
+            case "Beginner" -> 1;
+            case "Intermediate" -> 2;
+            case "Expert" -> 3;
+            default -> 0;
+        }; // Using String literals in Switch case
 
         // Printing the result
         System.out.println("Your Level is: " + level);
